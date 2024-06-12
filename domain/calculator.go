f88d91110de79a99ac228211d41e0a45f3df5c3a
aev26 <ast_egor@mail.ru>
2024-06-12 16:33:16 +0300

    Added Pow fucnction with tests

--- domain/calculator.go
+++ domain/calculator.go
@@ -1,6 +1,9 @@
 package calculator
 
-import "golang.org/x/exp/constraints"
+import (
+	"golang.org/x/exp/constraints"
+	"math"
+)
 
 type Number interface {
 	constraints.Integer | constraints.Float
@@ -20,4 +23,8 @@ func Mul[T Number](x, y T) T {
 
 func Div[T Number](x, y T) T {
 	return x / y
+}
+
+func Pow[T Number](x, y T) T {
+	return T(math.Pow(float64(x), float64(y)))
 }
